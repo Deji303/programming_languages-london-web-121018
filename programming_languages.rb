1cd21c6a74a2c8ab829eb9836aadeacf91d45887
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-26 17:55:02 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require 'pry'
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  new_hash = languages[:oo].merge(languages[:functional ])
+ 
+  new_hash.keys.each do |x|
+      languages[:oo].keys.each do |x|
+      new_hash[x][:style] = [:oo]
+      
+    end 
+    languages[:functional].keys.each do |x|
+      new_hash[x][:style] = [:functional]
+    end 
+    
+  end 
+  new_hash
 end
